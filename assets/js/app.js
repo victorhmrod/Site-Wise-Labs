79f6c51c0062c43266013b77a78c660a2d20a9c9
Victor Henrique <vhcgames1@gmail.com>
2023-05-25 20:03:44 -0300

    adasda

--- assets/js/app.js
+++ assets/js/app.js
@@ -118,7 +118,7 @@ let footer = $(`
       <!--SVG code for quote-left icon-->
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
-      <p class="tag" >Desenvolva uma paixão por aprender. Se o fizer, nunca deixará de crescer..</p>
+      <p class="tag" >Desenvolva uma paixão por aprender. Se o fizer, nunca deixará de crescer...</p>
     </div>
 
     
@@ -138,7 +138,7 @@ let footer = $(`
              <div class="form-header">
                 <h6 class="display">Entrar em contato</h6>
               </div>
-                <form name="form1" action="https://formcarry.com/s/BywEPAJNb" method="POST" accept-charset="UTF-8" >
+                <form name="form1" action="https://formcarry.com/s/HRokGZ82FR" method="POST" accept-charset="UTF-8" >
                   <input id="name" type="text" name="name" placeholder="Seu nome" required/>
                   <input id="email" type="email" name="email" placeholder="Endereço de Email" required/>                  
                   <textarea id="textArea" name="message" placeholder="Escreva sua mensagem" required></textarea>
