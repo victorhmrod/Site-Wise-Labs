6b10a48e6c11b6c36b15ce68ed8645cb396e2a05
victorhmrod <vhcgames1@gmail.com>
2025-01-08 14:34:33 -0300

    Add new projects to games page

--- assets/js/project.js
+++ assets/js/project.js
@@ -7,10 +7,29 @@ const projectcards = document.querySelector(".projectcards");
 const projects = [
   {
     title: "Frontline: New Revolution",
+    TitleLink: "https://store.steampowered.com/app/1772220/Frontline_New_Revolution/",
     cardImage: "assets/images/project-page/chess.jpg",
     description: "Grab your weapons and get ready to conquer the world! FRONTLINE is the INNOVATIVE 6v6 tactical TPS from Wise Labs!",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    reviewlink: "https://store.steampowered.com/app/1772220/Frontline_New_Revolution/",
+    Previewlink: "https://store.steampowered.com/app/1772220/Frontline_New_Revolution/",
+    Githublink: "https://linktr.ee/wiselabs.inc",
+  },
+  {
+    title: "Dissociation",
+    TitleLink: "#",
+    cardImage: "assets/images/project-page/book.png",
+    description: "An out-of-control AI makes all your fears come true, face your biggest fears as you try to decipher whether it's reality or not!",
+    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
+    Previewlink: "#",
+    Githublink: "https://linktr.ee/wiselabs.inc",
+  },
+  {
+    title: "Rampage Rush",
+    TitleLink: "#",
+    cardImage: "assets/images/project-page/quiz.jpg",
+    description: "Feel the beat as superstar Jack or Mia battles a megacorporation that controls the entire city under their father's rule using thunderous rhythmic combat!",
+    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
+    Previewlink: "#",
     Githublink: "https://linktr.ee/wiselabs.inc",
   }
 ];
@@ -18,7 +37,7 @@ const projects = [
 // function for rendering project cards data
 const showCards = () => {
   let output = "";
-  projects.forEach(({ title, cardImage, Previewlink, Githublink }) => {
+  projects.forEach(({title, TitleLink, cardImage, Previewlink, Githublink, description }) => {
        output += `       
         <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
           <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
@@ -27,8 +46,9 @@ const showCards = () => {
             <div class="data">
               <div class="content">
               <div class="title-div">
-                <h1 class="title"><a href="#">${title}</a></h1>
-                </div>
+                <h1 class="title"><a href="${TitleLink}">${title}</a></h1>
+              </div>
+                <a class="Description">${description}</a>
             <ul class="menu-content"><br>
                   <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
                   <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
